d9bcef1f05d625c81901facb1f8925ba9c7acbaf
Mosab Abudeeb <109781337+Mosabxly@users.noreply.github.com>
2026-02-05 14:19:34 +0200

    Initial Commit

---
+++ MID/MIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  MIDApp.swift
+//  MID
+//
+//  Created by mosab abudeeb on 05/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct MIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
